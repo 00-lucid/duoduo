3500fd6addc44b9554f65d06ddd8ff935f0a27a6
0xNSKY <namhj315@gmail.com>
2021-08-19 18:05:34 +0900

    init: react / tailwindcss / typescript

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,26 @@
-import "./App.css";
-import styled from "styled-components";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <TopMenu>
-        <h1 className="text-blue-500 font-bold">DuODuO</h1>
-        <h2>sign in</h2>
-      </TopMenu>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
-
-const TopMenu = styled.div({
-  width: "100%",
-  display: "flex",
-  flexDirection: "row",
-  color: "white",
-  alignItems: "center",
-  backgroundColor: "#282c34",
-});
 
 export default App;
