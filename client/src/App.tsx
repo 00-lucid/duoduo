8b827b07ea49bb88aa87f61d449a0f12b209a605
0xNSKY <namhj315@gmail.com>
2021-08-27 23:36:17 +0900

    fix: App

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,15 +3,20 @@ import styled from "styled-components";
 import { Route, Switch } from "react-router-dom";
 import Root from "./pages/Root";
 import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import { RecoilRoot } from "recoil";
 
 function App() {
   return (
-    <div className="App">
-      <Switch>
-        <Route path="/" exact component={Root}></Route>
-        <Route path="/signin" exact component={Signin}></Route>
-      </Switch>
-    </div>
+    <RecoilRoot>
+      <div className="App">
+        <Switch>
+          <Route path="/" exact component={Root}></Route>
+          <Route path="/signin" exact component={Signin}></Route>
+          <Route path="/signup" exact component={Signup}></Route>
+        </Switch>
+      </div>
+    </RecoilRoot>
   );
 }
 
