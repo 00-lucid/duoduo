2ed66b9c8b4cc7ccbd2767c38eb9924de7af893b
0xNSKY <namhj315@gmail.com>
2021-08-27 23:39:04 +0900

    fix: ServerApplication

--- server/src/main/java/com/duoduo/server/ServerApplication.java
+++ server/src/main/java/com/duoduo/server/ServerApplication.java
@@ -2,8 +2,12 @@ package com.duoduo.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan
+@EnableJpaRepositories
 public class ServerApplication {
 
 	public static void main(String[] args) {
