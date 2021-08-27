5f050221c39e0c302216aeeb8bac647893fe1216
0xNSKY <namhj315@gmail.com>
2021-08-27 23:38:08 +0900

    init: User Entity

--- server/src/main/java/com/duoduo/server/Entity/UserEntity.java
+++ server/src/main/java/com/duoduo/server/Entity/UserEntity.java
@@ -1,2 +1,35 @@
-package com.duoduo.server.Entity;public class UserEntity {
+package com.duoduo.server.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@Entity(name = "user")
+@NoArgsConstructor
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String email;
+
+    @Column
+    private String password;
+
+    @Column
+    private String nickname;
+
+    @Builder
+    public UserEntity(Long id, String email, String password, String nickname) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.nickname = nickname;
+    }
 }
