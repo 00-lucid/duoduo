47fc9b6c7a63af583220ef134e03b2d0e71f9b2c
0xNSKY <namhj315@gmail.com>
2021-11-26 08:36:24 +0900

    fix: userlist visual update + most champion

--- client/src/components/UserList.tsx
+++ client/src/components/UserList.tsx
@@ -3,10 +3,10 @@ import styled from "styled-components";
 function UserList({ room }: any) {
   return (
     <li
-      className="flex flex-row border h-20 text-white justify-start rounded-l-lg mb-2 overflow-hidden"
+      className="relative flex flex-row border h-20 text-white justify-start rounded-l-lg mb-2 overflow-hidden"
       style={{ backgroundColor: "#2b2d42" }}
     >
-      <section className="flex flex-row items-center">
+      <section className="flex flex-row items-center ">
         <div className="border-2 border-green-300 w-16 h-16 rounded-full m-2 overflow-hidden">
           <img
             src={`http://ddragon.leagueoflegends.com/cdn/11.21.1/img/profileicon/${room.profileIconId}.png`}
@@ -16,7 +16,7 @@ function UserList({ room }: any) {
           <p>{room.username}</p>
           <p className="opacity-50">{room.nickname}</p>
         </div>
-        <div className="w-16 h-16 ml-2">
+        <div className="w-14 h-14 ml-2">
           <img src={`./${room.position}.png`} />
         </div>
         <p className="w-4 m-3 font-black">{room.tier}</p>
@@ -25,12 +25,25 @@ function UserList({ room }: any) {
             <p className="text-sm">{room.recent_rate}%</p>
           </Graph>
         </div>
-        <div className="m-2 border w-40 h-16"></div>
-        <div className="w-4 m-3">{room.kda}</div>
-        <div className="w-4 m-3">{room.poro}</div>
-        <div className="w-4 m-3">{room.synergy}</div>
+        <div className="flex flex-row m-2 border w-48 h-16">
+          <img
+            className="w-16 h-full"
+            src={`http://ddragon.leagueoflegends.com/cdn/11.23.1/img/champion/${room.most[0].championName}.png`}
+          ></img>
+          <img
+            className="w-16 h-full"
+            src={`http://ddragon.leagueoflegends.com/cdn/11.23.1/img/champion/${room.most[1].championName}.png`}
+          ></img>
+          <img
+            className="w-16 h-full"
+            src={`http://ddragon.leagueoflegends.com/cdn/11.23.1/img/champion/${room.most[2].championName}.png`}
+          ></img>
+        </div>
+        {/* <div className="w-4 m-3">{room.kda}</div> */}
+        {/* <div className="w-4 m-3">{room.poro}</div> */}
+        {/* <div className="w-4 m-3">{room.synergy}</div> */}
         <div className="w-4 m-3">{room.total_rate}%</div>
-        <button className="bg-green-400 ml-10 w-10 h-full flex flex-row items-center justify-center">
+        <button className="absolute bg-green-400 ml-10 w-10 h-full flex flex-row items-center justify-center inset-y-0 right-0">
           <img className="w-6 h-6" src="icon_arrow.png"></img>
         </button>
       </section>
@@ -42,6 +55,9 @@ const Graph = styled.div<{ rate: string }>`
   width: ${(props) => (props.rate ? `${props.rate}%` : "0%")};
   height: 100%;
   background-color: #34d399;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 export default UserList;
