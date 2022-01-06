468056e5bd0d01bd8c0bb9aec5aca359955a2769
0xNSKY <namhj315@gmail.com>
2022-01-06 20:10:30 +0900

    fix: interface for socket

--- client/src/state.ts
+++ client/src/state.ts
@@ -9,6 +9,25 @@ interface Alarm {
   type: number;
 }
 
+interface ServerToClientEvents {
+  noArg: () => void;
+  basicEmit: (a: number, b: string, c: Buffer) => void;
+  withAck: (d: string, callback: (e: number) => void) => void;
+}
+
+interface ClientToServerEvents {
+  hello: () => void;
+}
+
+interface InterServerEvents {
+  ping: () => void;
+}
+
+interface SocketData {
+  name: string;
+  age: number;
+}
+
 const isLoadingState = atom({
   key: "isLoading",
   default: false,
